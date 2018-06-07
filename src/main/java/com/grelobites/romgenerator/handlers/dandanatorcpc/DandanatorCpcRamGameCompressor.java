141236a8392edd89f857fbf3f08e99ac02f45e86
Manuel Teira <manuel.teira@gmail.com>
2018-06-07 13:08:55 +0200

    Change Game compression rules

--- src/main/java/com/grelobites/romgenerator/handlers/dandanatorcpc/DandanatorCpcRamGameCompressor.java
+++ src/main/java/com/grelobites/romgenerator/handlers/dandanatorcpc/DandanatorCpcRamGameCompressor.java
@@ -11,11 +11,7 @@ import java.io.OutputStream;
 import java.util.Arrays;
 
 public class DandanatorCpcRamGameCompressor implements RamGameCompressor {
-    private static final int COMPRESSED_SLOT_THRESHOLD = Constants.SLOT_SIZE - 6;
-    private static final int COMPRESSED_CHUNKSLOT_THRESHOLD = Constants.SLOT_SIZE -
-            DandanatorCpcConstants.GAME_CHUNK_SIZE;
-    private static final int DELTA_LIMITED_SLOT = 1;
-    private static final int DELTA_LIMITED_SLOT_MAXDELTA = 16;
+    private static final int COMPRESSED_SLOT_THRESHOLD = Constants.SLOT_SIZE;
 
     private Compressor compressor = DandanatorCpcConfiguration.getInstance().getCompressor();
     private Integer compressionDelta;
@@ -40,25 +36,13 @@ public class DandanatorCpcRamGameCompressor implements RamGameCompressor {
     }
 
     private byte[] filterCompression(byte[] data, byte[] compressedData, int slot) {
-        if (slot == DELTA_LIMITED_SLOT) {
-            return compressionDelta > DELTA_LIMITED_SLOT_MAXDELTA ?
-                    data : compressedData;
-        }
-        if (slot == DandanatorCpcConstants.GAME_CHUNK_SLOT) {
-            return compressedData.length > COMPRESSED_CHUNKSLOT_THRESHOLD ?
-                    data : compressedData;
-        } else {
-            return compressedData.length > COMPRESSED_SLOT_THRESHOLD ?
-                    data : compressedData;
-        }
+        return compressedData.length > COMPRESSED_SLOT_THRESHOLD ?
+                data : compressedData;
     }
 
     @Override
     public byte[] compressSlot(int slot, byte[] data) {
-        byte[] targetData = (slot == DandanatorCpcConstants.GAME_CHUNK_SLOT) ?
-                Arrays.copyOfRange(data, 0, Constants.SLOT_SIZE - DandanatorCpcConstants.GAME_CHUNK_SIZE) :
-                data;
-        return filterCompression(targetData, compressSlotInternal(targetData), slot);
+        return filterCompression(data, compressSlotInternal(data), slot);
     }
 
 }
