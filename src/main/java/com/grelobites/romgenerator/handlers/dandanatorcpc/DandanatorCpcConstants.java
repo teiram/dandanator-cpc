08dc955a4c9408d606244bd35a83d175223d5e12
mteira <manuel.teira@gmail.com>
2018-06-08 10:57:50 +0200

    Change Chunk location and size

--- src/main/java/com/grelobites/romgenerator/handlers/dandanatorcpc/DandanatorCpcConstants.java
+++ src/main/java/com/grelobites/romgenerator/handlers/dandanatorcpc/DandanatorCpcConstants.java
@@ -36,8 +36,8 @@ public class DandanatorCpcConstants {
     private static final String EXTRA_ROM_RESOURCE = "dandanator-cpc/test.rom";
 
     public static final int POKE_TARGET_ADDRESS = 49284;
-    public static final int GAME_CHUNK_SIZE = 16;
-    public static final int GAME_CHUNK_SLOT = 2;
+    public static final int GAME_CHUNK_SIZE = 32;
+    public static final int GAME_CHUNK_SLOT = 3;
     public static final int VERSION_SIZE = 8;
     public static final int FILLER_BYTE = 0xFF;
     public static final int EXTENDED_CHARSET_SIZE = 896;
