ff113b58b324311c4a3112b885dadb0cc70dcf76
Manuel Teira <manuel.teira@gmail.com>
2025-05-02 20:31:46 +0200

    Linux specific baudrate settings

--- src/main/java/com/grelobites/romgenerator/util/SerialPortConfiguration.java
+++ src/main/java/com/grelobites/romgenerator/util/SerialPortConfiguration.java
@@ -1,28 +1,33 @@
 package com.grelobites.romgenerator.util;
 
+import com.sun.javafx.PlatformUtil;
 import jssc.SerialPort;
 import jssc.SerialPortException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public enum SerialPortConfiguration {
-    MODE_115200(114000, //Due to 464 clock jitter
+    MODE_115200(114200, //Due to 464 clock jitter
+            SerialPort.BAUDRATE_115200,
             SerialPort.DATABITS_8,
             SerialPort.STOPBITS_2,
             SerialPort.PARITY_NONE),
     MODE_57600(SerialPort.BAUDRATE_57600,
+            SerialPort.BAUDRATE_57600,
             SerialPort.DATABITS_8,
             SerialPort.STOPBITS_2,
             SerialPort.PARITY_NONE);
     private static final Logger LOGGER = LoggerFactory.getLogger(SerialPortConfiguration.class);
 
     public int baudrate;
+    public int baudrateLinux;
     public int dataBits;
     public int stopBits;
     public int parity;
 
-    SerialPortConfiguration(int baudrate, int dataBits, int stopBits, int parity) {
+    SerialPortConfiguration(int baudrate, int baudrateLinux, int dataBits, int stopBits, int parity) {
         this.baudrate = baudrate;
+        this.baudrateLinux = baudrateLinux;
         this.dataBits = dataBits;
         this.stopBits = stopBits;
         this.parity = parity;
@@ -30,13 +35,14 @@ public enum SerialPortConfiguration {
 
     public void apply(SerialPort serialPort) throws SerialPortException {
         LOGGER.debug("Applying serial port configuration {}", this);
-        serialPort.setParams(baudrate, dataBits, stopBits, parity);
+        boolean linux = PlatformUtil.isLinux();
+        serialPort.setParams(linux ? baudrateLinux : baudrate, dataBits, stopBits, parity);
     }
-
     @Override
     public String toString() {
         return "SerialPortConfiguration{" +
                 "baudrate=" + baudrate +
+                ", baudrateLinux=" + baudrateLinux +
                 ", dataBits=" + dataBits +
                 ", stopBits=" + stopBits +
                 ", parity=" + parity +
